18f4299e6ceaaafd23a197069c89078bd13405fe
Boiar Qin <boiar.qin@baltosoftware.com>
2022-12-02 16:37:47 -0600

    Add users for real

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -24,45 +24,13 @@ async function feed(parent, args, context, info) {
   };
 }
 
-// async function users(parent, args, context, info) {
-//   const users = await context.prisma.user.findMany({
-//     // where,
-//     // skip: args.skip,
-//     // take: args.take,
-//     // orderBy: args.orderBy
-//   });
-
-//   console.log(users)
-
-//   return {
-//     users
-//   }
-// }
-
-const users2 = {
-  id: '9b912a7f-8c7a-419e-91a7-d2c9d3d9f58e',
-  name: 'Jack',
-  email: 'jack@fakemail.com',
-  password: 'jack123'
-};
-
-const users = [
-  {
-    id: '9b912a7f-8c7a-419e-91a7-d2c9d3d9f58e',
-    name: 'Jack',
-    email: 'jack@fakemail.com',
-    password: 'jack123'
-  },
-  {
-    id: '089bae1e-884b-46e4-8d2d-92a2ee6efb6c',
-    name: 'Liz',
-    email: 'Liz@fakemail.com',
-    password: 'liz123'
-  }
-]
+async function users(parent, args, context, info) {
+  const users = await context.prisma.user.findMany({
+  });
+  return users  
+}
 
 module.exports = {
   feed,
-  user: () => users2,
-  users: () => users
+  users, 
 };

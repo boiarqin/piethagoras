70462ecd7f4ec6275d31d3eaadd82e7242342c58
Boiar Qin <boiarqin@gmail.com>
2022-12-14 14:55:03 -0600

    menu

--- client-next/pages/menu.js
+++ client-next/pages/menu.js
@@ -1,10 +1,10 @@
 import BlogPost from "../layouts/blog-post";
-import styles from "../styles/pages/Menu.module.css";
 import { SIZES, SAUCE, CRUST, CHEESE_AMOUNT, TOPPINGS, FAVORITES } from "../constants/pizza-options";
+import styles from "../styles/pages/Menu.module.css";
 
 const BYOCategory = ({categoryName, categoryOptions}) => {
     return (
-        <div className="byo-category">
+        <div className={`${styles['byo-category']} ${categoryName === 'Size' ? styles['byo-category-size'] : ''}`}>
         <h3>{categoryName}</h3>
             <ul>
                 {
@@ -13,7 +13,7 @@ const BYOCategory = ({categoryName, categoryOptions}) => {
 
                         return (<li key={item.key}>
                             <h4>{item.displayName}</h4>
-                            <p>{item.description}</p>
+                            <p><em>{item.description}</em></p>
                         </li>
                         )
                     })
@@ -27,21 +27,21 @@ const Post = () => {
     return (
         <BlogPost>
             <h1>Our Menu</h1>
-            <p>Choose from one of our favorites or build your own!</p>
+            <p><em>Choose from one of our favorites or build your own!</em></p>
 
             <h2>Favorite Pizzas</h2>
-            <ul className="favorites">
+            <ul className={styles.favorites}>
                 {
                     FAVORITES.map((pizza) => {
                         const toppings = pizza.toppings.map((topping) => topping.displayName).join(', ')
 
                         return (
-                            <li key={pizza.displayName}>
+                            <li key={pizza.displayName} className={styles.card}>
                                 <h3>{pizza.displayName}</h3>
-                                <p>{pizza.description}</p>
-                                <p>Crust: {pizza.crust.displayName}</p>
-                                <p>Sauce: {pizza.sauce.displayName}</p>
-                                <p>Toppings: {toppings}</p>
+                                <p><em>{pizza.description}</em></p>
+                                <p><strong>Crust:</strong> {pizza.crust.displayName}</p>
+                                <p><strong>Sauce:</strong> {pizza.sauce.displayName}</p>
+                                <p><strong>Toppings:</strong> {toppings}</p>
                             </li>
                         )
                     })
@@ -49,11 +49,24 @@ const Post = () => {
             </ul>
 
             <h2>Build Your Own Pizza</h2>
-            <BYOCategory categoryName="Size" categoryOptions={SIZES} />
+            <p><em>Have it your way!</em></p>
+            <div className={styles.byo}>
+            <div className={styles.wrapper}>
+                <div className={styles.left}>
+                <BYOCategory categoryName="Size" categoryOptions={SIZES} />
             <BYOCategory categoryName="Crust Options" categoryOptions={CRUST} />
-            <BYOCategory categoryName="Sauces" categoryOptions={SAUCE} />
+            
             <BYOCategory categoryName="Cheese Amount" categoryOptions={CHEESE_AMOUNT} />
-            <BYOCategory categoryName="Toppings" categoryOptions={TOPPINGS} />
+            <BYOCategory categoryName="Sauces" categoryOptions={SAUCE} />
+                </div>
+                <div className={styles.right}>
+                
+                <BYOCategory categoryName="Toppings" categoryOptions={TOPPINGS} />
+                </div>
+            </div>
+            </div>
+            
+            
 
         </BlogPost>
     )

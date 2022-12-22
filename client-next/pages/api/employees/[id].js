f6b5f5daf5872e6db37a5fe6f8a1c769d119729c
Boiar Qin <boiarqin@gmail.com>
2022-12-22 11:06:32 -0600

    Add errors to API

--- client-next/pages/api/employees/[id].js
+++ client-next/pages/api/employees/[id].js
@@ -3,5 +3,11 @@ import { EMPLOYEES } from "./data";
 export default function handler(req, res) {
     const {id} = req.query;
 
-    res.status(200).json({ data: EMPLOYEES.find(emp => emp.id === parseInt(id))});
+    const employeeData = EMPLOYEES.find(emp => emp.id === parseInt(id));
+
+    if (employeeData) {
+        res.status(200).json({ data: employeeData});
+    } else {
+        res.status(404).json({ error: {message: 'Employee Not Found'} });
+    }   
 }
--- client-next/pages/api/inventory/[id].js
+++ client-next/pages/api/inventory/[id].js
@@ -3,5 +3,11 @@ import { INVENTORY } from "./data";
 export default function handler(req, res) {
     const {id} = req.query;
 
-    res.status(200).json({ data: INVENTORY.find(inv => inv.id === parseInt(id))});
+    const itemData = INVENTORY.find(inv => inv.id === parseInt(id));
+
+    if (itemData) {
+        res.status(200).json({ data: itemData});
+    } else {
+        res.status(404).json({ error: {message: 'Not Found'} });
+    }
 }

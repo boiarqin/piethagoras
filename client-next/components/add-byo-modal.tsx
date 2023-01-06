a1139f4e868043a776ec780af97251cb4c0cfae4
Boiar Qin <boiarqin@gmail.com>
2023-01-06 15:54:00 -0600

    Style order modals

--- client-next/components/add-byo-modal.tsx
+++ client-next/components/add-byo-modal.tsx
@@ -10,6 +10,8 @@ import {
   DELIVERY_MODE,
   CARRYOUT_MODE,
 } from "../constants/pizza-options";
+import modalStyles from "../styles/components/Modal.module.css";
+import formStyles from "../styles/components/Form.module.css";
 
 interface Props {
   isModalOpen: boolean;
@@ -66,98 +68,127 @@ const AddBYOModal = ({ isModalOpen, onCloseModal, addPizzaToCart }: Props) => {
       // onAfterOpen={afterOpenModal}
       // onRequestClose={closeModal}
       // style={customStyles}
+      className={modalStyles.modal}
       contentLabel="Build Your Own Pizza"
     >
-      <h2>Build Your Own Pizza</h2>
+      <div className={modalStyles.header}>
+        <h2 className={modalStyles.title}>Build Your Own Pizza</h2>
+        <button
+          className={modalStyles["close-button"]}
+          onClick={closeModal}
+          aria-label="Close Modal"
+        >
+          X
+        </button>
+      </div>
 
-      <p>
-        <em>Have it your way!</em>
-      </p>
-      <form>
-        <label>
-          Choose size:
-          <select onChange={(e) => setSize(e.target.value)} value={size}>
-            {Object.keys(SIZES).map((key) => {
-              const size = SIZES[key];
+      <div className={modalStyles.content}>
+        <p>
+          <em>Have it your way!</em>
+        </p>
+        <form className={formStyles.form}>
+          <div className={formStyles["form-field"]}>
+            <label>
+              Choose size:
+              <select onChange={(e) => setSize(e.target.value)} value={size}>
+                {Object.keys(SIZES).map((key) => {
+                  const size = SIZES[key];
 
-              return (
-                <option key={size.key} value={size.key}>
-                  {size.displayName} - {size.description}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <label>
-          Choose crust:
-          <select onChange={(e) => setCrust(e.target.value)} value={crust}>
-            {Object.keys(CRUST).map((key) => {
-              const crust = CRUST[key];
-
-              return (
-                <option key={crust.key} value={crust.key}>
-                  {crust.displayName} - {crust.description}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <label>
-          Choose sauce:
-          <select onChange={(e) => setSauce(e.target.value)} value={sauce}>
-            {Object.keys(SAUCE).map((key) => {
-              const sauce = SAUCE[key];
-
-              return (
-                <option key={sauce.key} value={sauce.key}>
-                  {sauce.displayName} - {sauce.description}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <label>
-          Choose cheese amount:
-          <select
-            onChange={(e) => setCheeseAmount(e.target.value)}
-            value={cheeseAmount}
-          >
-            {Object.keys(CHEESE_AMOUNT).map((key) => {
-              const amt = CHEESE_AMOUNT[key];
-
-              return (
-                <option key={amt.key} value={amt.key}>
-                  {amt.displayName} - {amt.description}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <fieldset>
-          <legend>Choose toppings:</legend>
-          <div>
-            {Object.keys(TOPPINGS).map((key) => {
-              const topping = TOPPINGS[key];
-
-              return (
-                <div key={topping.key}>
-                  <input
-                    type="checkbox"
-                    id={`toppings-${topping.key}`}
-                    value={topping.key}
-                    onClick={changeToppings}
-                  />
-                  <label htmlFor={`toppings-${topping.key}`}>
-                    {topping.displayName} - {topping.description}
-                  </label>
-                </div>
-              );
-            })}
+                  return (
+                    <option key={size.key} value={size.key}>
+                      {size.displayName} - {size.description}
+                    </option>
+                  );
+                })}
+              </select>
+            </label>
           </div>
-        </fieldset>
-      </form>
-      <button onClick={addToCart}>Add to Cart</button>
-      <button onClick={closeModal}>close</button>
+          <div className={formStyles["form-field"]}>
+            <label>
+              Choose crust:
+              <select onChange={(e) => setCrust(e.target.value)} value={crust}>
+                {Object.keys(CRUST).map((key) => {
+                  const crust = CRUST[key];
+
+                  return (
+                    <option key={crust.key} value={crust.key}>
+                      {crust.displayName} - {crust.description}
+                    </option>
+                  );
+                })}
+              </select>
+            </label>
+          </div>
+          <div className={formStyles["form-field"]}>
+            <label>
+              Choose sauce:
+              <select onChange={(e) => setSauce(e.target.value)} value={sauce}>
+                {Object.keys(SAUCE).map((key) => {
+                  const sauce = SAUCE[key];
+
+                  return (
+                    <option key={sauce.key} value={sauce.key}>
+                      {sauce.displayName} - {sauce.description}
+                    </option>
+                  );
+                })}
+              </select>
+            </label>
+          </div>
+          <div className={formStyles["form-field"]}>
+            <label>
+              Choose cheese amount:
+              <select
+                onChange={(e) => setCheeseAmount(e.target.value)}
+                value={cheeseAmount}
+              >
+                {Object.keys(CHEESE_AMOUNT).map((key) => {
+                  const amt = CHEESE_AMOUNT[key];
+
+                  return (
+                    <option key={amt.key} value={amt.key}>
+                      {amt.displayName} - {amt.description}
+                    </option>
+                  );
+                })}
+              </select>
+            </label>
+          </div>
+          <fieldset className={formStyles.fieldset}>
+            <legend className={formStyles.legend}>Choose toppings:</legend>
+            <div>
+              {Object.keys(TOPPINGS).map((key) => {
+                const topping = TOPPINGS[key];
+
+                return (
+                  <div
+                    key={topping.key}
+                    className={formStyles["checkbox-option"]}
+                  >
+                    <input
+                      type="checkbox"
+                      id={`toppings-${topping.key}`}
+                      value={topping.key}
+                      onChange={changeToppings}
+                    />
+                    <label
+                      className={formStyles["checkbox-label"]}
+                      htmlFor={`toppings-${topping.key}`}
+                    >
+                      {topping.displayName} - {topping.description}
+                    </label>
+                  </div>
+                );
+              })}
+            </div>
+          </fieldset>
+        </form>
+      </div>
+      <div className={modalStyles.footer}>
+        <button className="primary" onClick={addToCart}>
+          Add to Cart
+        </button>
+      </div>
     </Modal>
   );
 };
--- client-next/components/add-favorites-modal.tsx
+++ client-next/components/add-favorites-modal.tsx
@@ -3,6 +3,9 @@ import Modal from "react-modal";
 import { SIZES, CHEESE_AMOUNT } from "../constants/pizza-options";
 import type { PresetPizza } from "../types/pizza.types";
 
+import modalStyles from "../styles/components/Modal.module.css";
+import formStyles from "../styles/components/Form.module.css";
+
 interface Props {
   isModalOpen: boolean;
   selectedPizza: PresetPizza;
@@ -46,64 +49,83 @@ const AddFavoritesModal = ({
       // onAfterOpen={afterOpenModal}
       // onRequestClose={closeModal}
       // style={customStyles}
+      className={modalStyles.modal}
       contentLabel="Finish customizing your pizza"
     >
-      <h2>{selectedPizza.displayName}</h2>
+      <div className={modalStyles.header}>
+        <h2 className={modalStyles.title}>{selectedPizza.displayName}</h2>
+        <button
+          className={modalStyles["close-button"]}
+          onClick={closeModal}
+          aria-label="Close Modal"
+        >
+          X
+        </button>
+      </div>
 
-      <p>
-        <em>{selectedPizza.description}</em>
-      </p>
-      <p>
-        <strong>Crust:</strong> {selectedPizza.crust.displayName}
-      </p>
-      <p>
-        <strong>Sauce:</strong> {selectedPizza.sauce.displayName}
-      </p>
-      <p>
-        <strong>Toppings:</strong>{" "}
-        {selectedPizza.toppings
-          .map((topping) => topping.displayName)
-          .join(", ")}
-      </p>
+      <div className={modalStyles.content}>
+        <p>
+          <em>{selectedPizza.description}</em>
+        </p>
+        <p>
+          <strong>Crust:</strong> {selectedPizza.crust.displayName}
+        </p>
+        <p>
+          <strong>Sauce:</strong> {selectedPizza.sauce.displayName}
+        </p>
+        <p>
+          <strong>Toppings:</strong>{" "}
+          {selectedPizza.toppings
+            .map((topping) => topping.displayName)
+            .join(", ")}
+        </p>
 
-      <form>
-        <label>
-          Choose size:
-          <select
-            onChange={(e) => setSelectedSize(e.target.value)}
-            value={selectedSize}
-          >
-            {Object.keys(SIZES).map((key) => {
-              const size = SIZES[key];
+        <form className={formStyles.form}>
+          <div className={formStyles["form-field"]}>
+            <label>
+              Choose size:
+              <select
+                onChange={(e) => setSelectedSize(e.target.value)}
+                value={selectedSize}
+              >
+                {Object.keys(SIZES).map((key) => {
+                  const size = SIZES[key];
 
-              return (
-                <option key={size.key} value={size.key}>
-                  {size.displayName} - {size.description}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <label>
-          Choose cheese amount:
-          <select
-            onChange={(e) => setSelectedCheeseAmt(e.target.value)}
-            value={selectedCheeseAmt}
-          >
-            {Object.keys(CHEESE_AMOUNT).map((key) => {
-              const amt = CHEESE_AMOUNT[key];
+                  return (
+                    <option key={size.key} value={size.key}>
+                      {size.displayName} - {size.description}
+                    </option>
+                  );
+                })}
+              </select>
+            </label>
+          </div>
+          <div className={formStyles["form-field"]}>
+            <label>
+              Choose cheese amount:
+              <select
+                onChange={(e) => setSelectedCheeseAmt(e.target.value)}
+                value={selectedCheeseAmt}
+              >
+                {Object.keys(CHEESE_AMOUNT).map((key) => {
+                  const amt = CHEESE_AMOUNT[key];
 
-              return (
-                <option key={amt.key} value={amt.key}>
-                  {amt.displayName} - {amt.description}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-      </form>
-      <button onClick={addToCart}>Add to Cart</button>
-      <button onClick={closeModal}>close</button>
+                  return (
+                    <option key={amt.key} value={amt.key}>
+                      {amt.displayName} - {amt.description}
+                    </option>
+                  );
+                })}
+              </select>
+            </label>
+          </div>
+        </form>
+      </div>
+      <div className={modalStyles.footer}>
+        <button className="primary" onClick={addToCart}>
+          Add to Cart
+        </button>
+      </div>
     </Modal>
   );
 };
